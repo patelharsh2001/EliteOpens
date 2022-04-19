d007a69149b4e2414a90973196a18a82753483c7
Jin Jong Yoo <jjyoo03@gmail.com>
2022-04-19 14:13:27 -0400

    website icon change

--- Client/src/app/pages/home/home.component.ts
+++ Client/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BasepageComponent } from '../../partials/basepage/basepage.component';
 import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,10 +11,12 @@ export class HomeComponent extends BasepageComponent implements OnInit {
 
   constructor(route:ActivatedRoute) { 
   super(route);
-
   }
+  
 
   override ngOnInit(): void {
   }
+
+  
 
 }
